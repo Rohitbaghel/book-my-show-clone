43b5e20500a0c081206d66fb5693f8317ddebf78
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2022-01-18 18:54:16 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-   <h1 className="text-3xl font-bold underline border">
-      Hello world!
-    </h1>
+  <Footer/>
     </div>
   );
 }
