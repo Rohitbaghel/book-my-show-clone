6425f38018c2dc06792ab4b97199f7142835b55b
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2022-01-19 11:17:10 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Home } from './components/Home';
+import Footer from './components/Footer';
+import Navbar from './components/navbar.components';
+
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Navbar />
+     
+  <Footer/>
     </div>
   );
 }
